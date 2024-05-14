372b1f23ac4213284a60eca5ff6b171faeb9c594
joao batista <joaobatysta1975@gmail.com>
2024-05-14 18:36:22 -0300

    Erro a executar o spring tool

---
+++ src/main/java/com/batystasilva/workshopmongo/services/UserService.java
@@ -0,0 +1,25 @@
+package com.batystasilva.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.batystasilva.workshopmongo.domain.User;
+import com.batystasilva.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	
+	@Autowired
+	private UserRepository repo;
+	
+
+	public List<User> findAll(){
+		return repo.findAll();
+		
+		
+	}
+
+}
